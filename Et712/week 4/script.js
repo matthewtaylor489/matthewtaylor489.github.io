0a51c37ac22eeeba9a3eeb38589c3bae36707f4b
matthewtaylor489 <92065087+matthewtaylor489@users.noreply.github.com>
2021-10-13 16:14:30 -0700

    Add files via upload

---
+++ Et712/week 4/script.js
@@ -0,0 +1,22 @@
+console.log("It's getting colder! Let's cover the tempature here, 20°C is 68°f");
+
+console.log("It's getting colder! Let's cover the tempature here, 76°f is 24.4°c")
+
+
+let johnheight=24
+let johnweight=600
+
+let lucasheight=67
+let lucasweight=467
+
+let lucasbmi=0.5
+let johnbmi=5.1
+
+  if (johnbmi>lucasbmi) { console.log('johns bmi is higher than lucas bmi')
+    
+  } 
+  else if (lucasbmi<johnbmi) {
+    console.log('lucas bmi is less than johns bmi')
+    
+  }
+
