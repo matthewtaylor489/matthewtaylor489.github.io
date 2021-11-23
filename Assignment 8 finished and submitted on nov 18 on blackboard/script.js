d902908380e657b4c8e95589d506b05cfd60e485
matthewtaylor489 <92065087+matthewtaylor489@users.noreply.github.com>
2021-11-23 07:51:27 -0800

    Add files via upload

---
+++ Assignment 8 finished and submitted on nov 18 on blackboard/script.js
@@ -0,0 +1,29 @@
+let numbers = ['2','8','32','128','512','2048'
+];
+
+for (let k=0; k<numbers.length; k++) {
+    console.log(numbers[k]);
+
+  
+}
+
+for (let F=numbers.length-2; F>0; F--) {
+    console.log(numbers[F]);
+
+  
+}
+
+
+const bills= ['24','36','204','80','182','440','100','10','1008','48'
+];
+
+let totalammount=2132;
+
+for (p=0; p<bills.length; ++p) 
+{
+
+console.log(totalammount);
+}
+
+
+
