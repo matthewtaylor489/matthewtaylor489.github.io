7f4cf093aae95d2f9eadaa17f569ac40f80cb3bb
matthewtaylor489 <92065087+matthewtaylor489@users.noreply.github.com>
2021-11-26 10:46:40 -0800

    assignment 6

---
+++ assignment 6/script.js
@@ -0,0 +1,43 @@
+
+
+let calcaverage=(knicksscore) => { return "13.8";
+
+(netsscore)=> {return "16.8"}
+}
+
+console.log('netsscore is higher than knicksscore')
+
+const knicksscore= ['44','23','71']
+
+
+
+const Netsscore= ['65','54','49']
+
+let avgnets=16.8
+
+let avgknicks=13.8
+
+
+
+let johnheight=1.95
+
+let johnweight=92
+
+let lucasheight=1.69
+
+let lucasweight=78
+
+let lucasbmi=0.036
+
+let johnbmi=0.041
+
+  if (johnbmi>lucasbmi) { console.log('johns bmi is higher than lucas bmi');
+    
+  } 
+  else if (lucasbmi<johnbmi) {
+    console.log('lucas bmi is less than johns bmi');
+    
+  }
+
+
+ 
